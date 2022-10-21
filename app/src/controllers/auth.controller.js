14b2c457b5417e669b8bcadb562081bdeeb23918
saichidvi <saichidvigupta@gmail.com>
2022-10-21 14:08:55 +0530

    created the controllers loginWithPhoneOtp

--- app/src/controllers/auth.controller.js
+++ app/src/controllers/auth.controller.js
@@ -15,6 +15,8 @@ const { checkPassword, hashPassword } = require("../utils/password.util");
 const { createJwtToken } = require("../utils/token.util");
 
 
+// --------------------- create new user --------------------------------
+
 exports.createNewUser = async (req, res, next) => {
     try {
       let { phone, name } = req.body;
@@ -65,4 +67,124 @@ exports.createNewUser = async (req, res, next) => {
       next(error);
     }
   };
+
+
+
+  // ------------ login with phone otp ----------------------------------
+
+exports.loginWithPhoneOtp = async (req, res, next) => {
+    try {
+  
+      const { phone } = req.body;
+      const user = await User.findOne({ phone });
+  
+      if (!user) {
+        next({ status: 400, message: PHONE_NOT_FOUND_ERR });
+        return;
+      }
+  
+      res.status(201).json({
+        type: "success",
+        message: "OTP sended to your registered phone number",
+        data: {
+          userId: user._id,
+        },
+      });
+  
+      // generate otp
+      const otp = generateOTP(6);
+      // save otp to user collection
+      user.phoneOtp = otp;
+      user.isAccountVerified = true;
+      await user.save();
+      // send otp to phone number
+      await fast2sms(
+        {
+          message: `Your OTP is ${otp}`,
+          contactNumber: user.phone,
+        },
+        next
+      );
+    } catch (error) {
+      next(error);
+    }
+  };
+
+
+  // ---------------------- verify phone otp -------------------------
+
+  exports.verifyPhoneOtp = async (req, res, next) => {
+    try {
+      const { otp, userId } = req.body;
+      const user = await User.findById(userId);
+      if (!user) {
+        next({ status: 400, message: USER_NOT_FOUND_ERR });
+        return;
+      }
+  
+      if (user.phoneOtp !== otp) {
+        next({ status: 400, message: INCORRECT_OTP_ERR });
+        return;
+      }
+      const token = createJwtToken({ userId: user._id });
+  
+      user.phoneOtp = "";
+      await user.save();
+  
+      res.status(201).json({
+        type: "success",
+        message: "OTP verified successfully",
+        data: {
+          token,
+          userId: user._id,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+
+
+  // --------------- fetch current user -------------------------
+
+exports.fetchCurrentUser = async (req, res, next) => {
+    try {
+      const currentUser = res.locals.user;
+  
+  
+      return res.status(200).json({
+        type: "success",
+        message: "fetch current user",
+        data: {
+          user:currentUser,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+
+// --------------- admin access only -------------------------
+exports.handleAdmin = async (req, res, next) => {
+    try {
+      const currentUser = res.locals.user;
+  
+      return res.status(200).json({
+        type: "success",
+        message: "Okay you are admin!!",
+        data: {
+          user:currentUser,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+
+
+
+
 
